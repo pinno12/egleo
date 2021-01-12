5bdb4cb1c0c7157c97d1f39af7c1e04e2463a624
wonny <pinno12@kaist.ac.kr>
2021-01-12 21:49:01 +0900

    add ad images

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 AOS.init();
 
 
+$('.tool-tip').popup();
+
+function bg_change(color) {
+    $(".ui.button.full-width.items").removeClass("bg-e");
+    document.getElementById(color).classList.add("bg-e");
+    // document.getElementById(color).style.backgroundColor = "#FFB81C !important";
+    document.getElementById("item-name").textContent=color;
+    document.getElementById("item-name2").textContent=color;  
+}
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
